a73e4691365e8b4584eeefc21dba49d04f65fd1e
briena-harmening <nrenc027@gmail.com>
2017-07-10 11:20:31 -0400

    drastic changes to style, use of tabs, mobile opt...

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-$( document ).ready(function(){
+$(document).ready(function() {
+$('.nav_scroll').click(function(event) {
+    var id = $(this).attr("href");
+    // var offset = 70;
+    var target = $(id).offset().top - 30;
+    $('html, body').animate({
+      scrollTop: target
+    }, 800);
+    event.preventDefault();
+  });
 
- $(".button-collapse").sideNav();
 
 
 
 
 
 
-})
+
+});
