298560d02f4a1a1b0e12f95945877609e28d130c
briena-harmening <nrenc027@gmail.com>
2017-07-16 16:34:50 -0400

    addition of gallery viws, still need to impliment slider or popup

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 $(document).ready(function() {
-$('.nav_scroll').click(function(event) {
-    var id = $(this).attr("href");
-    // var offset = 70;
-    var target = $(id).offset().top - 30;
-    $('html, body').animate({
-      scrollTop: target
-    }, 800);
-    event.preventDefault();
-  });
 
+  // var elem = document.querySelector('.m-p-g');
+  //
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   var gallery = new MaterialPhotoGallery(elem);
+  // });
 
+  var $overlay = $('<div id="overlay"></div>');
+  var $image = $("<img>");
 
+  //An image to overlay
+  $overlay.append($image);
 
+  //Add overlay
+  $("body").append($overlay);
 
+    //click the image and a scaled version of the full size image will appear
+    $(".m-p-g__thumbs-img").click( function(event) {
+      console.log("clicking IMG");
+      event.preventDefault();
+      var imageLocation = $(this).attr("data-full");
+
+      //update overlay with the image linked in the link
+      $image.attr("data-full", imageLocation);
+
+      //show the overlay
+      $overlay.show();
+    } );
+
+    $("#overlay").click(function() {
+      $("#overlay").hide();
+    });
 
 
 
